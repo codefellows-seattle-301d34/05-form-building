f010290905cb17b08d1dba3f2026aa33f026c54c
Jermaine Walker <jermainemwalker@gmail.com>
2018-05-13 21:51:03 -0700

    Started form in new.html.

--- jermaine-walker/scripts/articleView.js
+++ jermaine-walker/scripts/articleView.js
@@ -3,79 +3,79 @@
 let articleView = {};
 
 articleView.populateFilters = () => {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      let val = $(this).find('address a').text();
+  $( 'article' ).each( function () {
+    if ( !$( this ).hasClass( 'template' ) ) {
+      let val = $( this ).find( 'address a' ).text();
       let optionTag = `<option value="${val}">${val}</option>`;
 
-      if ($(`#author-filter option[value="${val}"]`).length === 0) {
-        $('#author-filter').append(optionTag);
+      if ( $( `#author-filter option[value="${val}"]` ).length === 0 ) {
+        $( '#author-filter' ).append( optionTag );
       }
 
-      val = $(this).attr('data-category');
+      val = $( this ).attr( 'data-category' );
       optionTag = `<option value="${val}">${val}</option>`;
-      if ($(`#category-filter option[value="${val}"]`).length === 0) {
-        $('#category-filter').append(optionTag);
+      if ( $( `#category-filter option[value="${val}"]` ).length === 0 ) {
+        $( '#category-filter' ).append( optionTag );
       }
     }
-  });
+  } );
 };
 
 articleView.handleAuthorFilter = () => {
-  $('#author-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $(`article[data-author="${$(this).val()}"]`).fadeIn();
+  $( '#author-filter' ).on( 'change', function () {
+    if ( $( this ).val() ) {
+      $( 'article' ).hide();
+      $( `article[data-author="${$( this ).val()}"]` ).fadeIn();
     } else {
-      $('article').fadeIn();
-      $('article.template').hide();
+      $( 'article' ).fadeIn();
+      $( 'article.template' ).hide();
     }
-    $('#category-filter').val('');
-  });
+    $( '#category-filter' ).val( '' );
+  } );
 };
 
 articleView.handleCategoryFilter = () => {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $(`article[data-category="${$(this).val()}"]`).fadeIn();
+  $( '#category-filter' ).on( 'change', function () {
+    if ( $( this ).val() ) {
+      $( 'article' ).hide();
+      $( `article[data-category="${$( this ).val()}"]` ).fadeIn();
     } else {
-      $('article').fadeIn();
-      $('article.template').hide();
+      $( 'article' ).fadeIn();
+      $( 'article.template' ).hide();
     }
-    $('#author-filter').val('');
-  });
+    $( '#author-filter' ).val( '' );
+  } );
 };
 
 articleView.handleMainNav = () => {
-  $('nav').on('click', '.tab', function(e) {
+  $( 'nav' ).on( 'click', '.tab', function ( e ) {
     e.preventDefault();
-    $('.tab-content').hide();
-    $(`#${$(this).data('content')}`).fadeIn();
-  });
+    $( '.tab-content' ).hide();
+    $( `#${$( this ).data( 'content' )}` ).fadeIn();
+  } );
 
-  $('nav .tab:first').click();
+  $( 'nav .tab:first' ).click();
 };
 
 articleView.setTeasers = () => {
-  $('.article-body *:nth-of-type(n+2)').hide();
-  $('article').on('click', 'a.read-on', function(e) {
+  $( '.article-body *:nth-of-type(n+2)' ).hide();
+  $( 'article' ).on( 'click', 'a.read-on', function ( e ) {
     e.preventDefault();
-    if ($(this).text() === 'Read on →') {
-      $(this).parent().find('*').fadeIn();
-      $(this).html('Show Less &larr;');
+    if ( $( this ).text() === 'Read on →' ) {
+      $( this ).parent().find( '*' ).fadeIn();
+      $( this ).html( 'Show Less &larr;' );
     } else {
-      $('body').animate({
-        scrollTop: ($(this).parent().offset().top)
-      },200);
-      $(this).html('Read on &rarr;');
-      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+      $( 'body' ).animate( {
+        scrollTop: ( $( this ).parent().offset().top )
+      }, 200 );
+      $( this ).html( 'Read on &rarr;' );
+      $( this ).parent().find( '.article-body *:nth-of-type(n+2)' ).hide();
     }
-  });
+  } );
 };
 
 // COMMENT: Where is this function called? Why?
-// PUT YOUR RESPONSE HERE
+// This function is called at the bottom of the index.html page, and its purpose is to fill the role of our document.ready() function, calling the HTML method on all of our articles and appending them to the DOM. It also calls all of the remaining methods.
 articleView.initNewArticlePage = () => {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
 
@@ -83,9 +83,9 @@ articleView.initNewArticlePage = () => {
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
 
-  $('#article-json').on('focus', function(){
+  $( '#article-json' ).on( 'focus', function () {
     this.select();
-  });
+  } );
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
 
@@ -103,7 +103,7 @@ articleView.create = () => {
 
 
   // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
-  $('pre code').each();
+  $( 'pre code' ).each();
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
 
@@ -112,7 +112,7 @@ articleView.create = () => {
 // COMMENT: Where is this function called? Why?
 // PUT YOUR RESPONSE HERE
 articleView.initIndexPage = () => {
-  articles.forEach(article => $('#articles').append(article.toHtml()));
+  articles.forEach( article => $( '#articles' ).append( article.toHtml() ) );
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
