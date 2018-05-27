bdfc53ae7f63f26d5e74f8f8f28188af7c233ef9
Sam Porter Giffords <sampgiffords@Sams-MacBook-Pro.local>
2018-05-27 14:24:48 -0700

    Adjusted code & answered comments in new & index.html files

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -75,12 +75,12 @@ articleView.setTeasers = () => {
 };
 
 // COMMENT: Where is this function called? Why?
-// PUT YOUR RESPONSE HERE
+// This is called at the end of new.html.  It starts up the article.View.newArticlePage function found just below. 
 articleView.newArticlePage = () => {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
 $('#write').show();
 
-  // TODO: The new articles we create will be copy/pasted into our source data file.
+  // DONE: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
 
   $('#article-export').hide();
@@ -88,17 +88,17 @@ $('#write').show();
   $('#article-json').on('focus', function(){
     this.select();
   });
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
 $('#new-form').on('change', 'input, text-area', articleView.create);
 };
 
 articleView.create = () => {
-  // TODO: Set up a variable to hold the new article we are creating.
+  // DONE: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
+    let article;
+    $('#articles').empty(); 
 
-    
-
-  // TODO: Instantiate an article based on what's in the form fields:
+  // DONE: Instantiate an article based on what's in the form fields:
    let newArticle = new Article({
      title: $('#article-title').val(),
      author: $('#article-author').val(),
@@ -109,30 +109,25 @@ articleView.create = () => {
      body: $('#article-body').val(),
    });
 
-  // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-  // THIS IS NOT WORKING RIGHT NOW
-  // THE FOLLOWING CODE NEEDS TO BE ADJUSTED
-  alert('title: ' + newArticle.title + '\nauthor: ' + newArticle.author + '\nauthor URL: ' + newArticle.authorUrl + '\ncategory: ' + newArticle.category + '\npublishedOn: ' + newArticle.publishedOn + '\nbody: ' + newArticle.body);
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+  // DONE: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
 
-  // $newArticle.on('checked', function (e) {
-  //   e.preventDefault();
-  //   let $newArticle = "#article-template".toHtml();
-  // };
-
-
-  // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  $('#articles').append(article.toHtml());
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-// "Think about what you know about JSON"
-// "Code that will run on change of focus"
-// "Use this as callback function up above as articleView.create =()"
-// Use articleView.create as callback function for articleView.initNewArticlePage
-  
-  //write info to article-export field and show the field (it was hidden on page init) 
+  // "Think about what you know about JSON"
+  // "Code that will run on change of focus"
+  // "Use this as callback function up above as articleView.create =()"
+  // Use articleView.create as callback function for articleView.initNewArticlePage
+  $('#export-field').show();
+  $('#article-json').val(`${JSON.stringify(article)},`);
 };
+
+  //write info to article-export field and show the field (it was hidden on page init) 
+
 
 // COMMENTED: Where is this function called? Why?
 // This function is called at the bottom of index.html.  It calls all the functions required to instantiate the articles, fill the filters for later use, add functionality for the nav buttons, and set the article view to "teaser" where the user has to click on the button to show the rest of the requested article. 
